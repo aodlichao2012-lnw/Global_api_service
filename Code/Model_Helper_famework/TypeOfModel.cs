265cb4e6c45570d772e62b55cdd2bddeed1b853c
aodlichao2012-lnw <52318749+aodlichao2012-lnw@users.noreply.github.com>
2023-10-19 15:04:07 +0700

    Create TypeOfModel.cs

---
+++ Code/Model_Helper_famework/TypeOfModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model_Helper_famework
+{
+    public class TypeOfModel
+    {
+        public int MyProperty { get; set; }
+        public string MyProperty1 { get; set; }
+        public double MyProperty2 { get; set; }
+        public long MyProperty3 { get; set; }
+        public Decimal MyProperty4 { get; set; }
+        public short MyProperty5 { get; set; }
+        public DateTime MyProperty6 { get; set; }
+    }
+}
